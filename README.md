90767cbb1af78a8123d6ba7656e25248698fd2e1
ryan.veitch <ryan.r.veitch@gmail.com>
2016-12-09 20:59:51 -0600

    /auth updates

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 destiny-oauth
+
+#### Reference
+- http://blog.mgechev.com/2014/02/19/create-https-tls-ssl-application-with-express-nodejs/
+- https://github.com/request/request/wiki/Using-request-with-https
--- start.js
+++ start.js
@@ -3,18 +3,12 @@
 
 var express = require('express');
 var request = require('request');
-
-
-//var fs = require('fs');
-//var sqlite3 = require('sqlite3').verbose();
+//var https = require('https'); // FOR LOCAL SSL ONLY
+var fs = require('fs');
+var sqlite3 = require('sqlite3').verbose();
 //var program = require('commander');
 var apiKey = require('./apiKey').getKey();
-	console.log('API Key: ' + apiKey);
 var authURL = require('./authURL').getAuthURL();
-	console.log('Authorization URL: ' + authURL);
-
-//Using request with https
-//https://github.com/request/request/wiki/Using-request-with-https
 var app = express();
 var port = Number(process.env.PORT || 3000);
 
@@ -22,28 +16,33 @@ app.set('json spaces', 2); // Set Express to pretty print json
 //app.set('view engine', 'ejs'); // Set Express's view engine to EJS
 
 var username, token, refreshToken;
+var authState = 'test';
 
 /******************** EXPRESS ROUTES ********************/
 
 // Default Endpoint
 app.get('/', function (req, res) {
-	/*var baseURL = req.protocol + '://' + req.headers.host;
+	var baseURL = req.protocol + '://' + req.headers.host;
 	var genericMessage = {
 		name: 'Destiny oAuth - Home',
 		url: baseURL,
-	}*/
-	res.redirect(302, 'https://www.bungie.net' + authURL + "?state=test");
-	//res.json(genericMessage);
+		endpoints: {
+			'/': baseURL + '/',
+			'/signin': baseURL + '/signin',
+			'/auth': baseURL + '/auth',
+		}
+	}
+	res.json(genericMessage);
 });
 
-// Default Endpoint
+// Sign-In Endpoint
+app.get('/signin', function (req, res) {
+	res.redirect(302, 'https://www.bungie.net' + authURL + "?state="+ authState );
+});
+
+// Auth Redirect Endpoint
 app.get('/auth', function (req, res) {
 	console.log(req.query);
-	console.log(req.query.code);
-
-	//https://destiny-oauth.herokuapp.com/auth?code=c0ccaa7563d07dd3caeedf76fa847dac&state=test
-	//93c9842bccf74b47cd0f3920641ba057
-	//var baseURL = req.protocol + '://' + req.headers.host;
 
   var post_data = {
     code: req.query.code,
@@ -61,24 +60,44 @@ app.get('/auth', function (req, res) {
 	    'X-API-Key': apiKey,
       //'Content-Type': 'application/json; charset=UTF-8', // set by json parameter
 	  },
-		json: post_data,
-		postData: {
-			mimeType: 'application/x-www-form-urlencoded',
-			params: [
-				{
-					name: 'foo',
-					value: 'bar'
-				},
-				{
-					name: 'hello',
-					value: 'world'
-				}
-			]
-		}
-	}, onRequestResponse);
+		json: {
+			'code': 'b36868244121de60c25a49f0c8ca7de9'
+		},
+	}, onRequestResponse);*/
 
-	function onRequestResponse(error, response, body) {
+	// generic function sending messages
+	//function sendMessage(recipientId, message) {
+	    request({
+	        url: 'https://www.bungie.net/Platform/App/GetAccessTokensFromCode/',
+	        //qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+	        method: 'POST',
+					headers: {
+						'Host': 'www.bungie.net',
+						'Accept': 'application/json',
+						'Origin': req.protocol + '://' + req.headers.host,
+						'User-Agent': 'app-platform',
+						'Content-Length': post_data.toString().length,
+				    'X-API-Key': apiKey,
+			      //'Content-Type': 'application/json; charset=UTF-8', // set by json parameter
+				  },
+	        json: {
+	            code: req.query.code,
+	        }
+	    }, function(error, response, body) {
+	        if (error) {
+	            console.log('Error sending message: ', error);
+	        } else if (response.body.error) {
+	            console.log('Error: ', response.body.error);
+	        } else {
+						console.log(response.body);
+						res.json(parsedResponse);
+					}
+	    });
+	//};
+
+	/*function onRequestResponse(error, response, body) {
 	  var parsedResponse = JSON.parse(body);
+		console.log('parsedResponse:');
 		console.log(parsedResponse);
 		res.json(parsedResponse);
 	}*/
@@ -89,12 +108,26 @@ app.get('/auth', function (req, res) {
 		name: 'Destiny oAuth - Authorization Redirect',
 		url: baseURL
 	}*/
-	res.json(post_data);
+	//res.json(post_data);
 });
 
 /******************************************************************************/
 
 // Server Listen
 app.listen(port, function () {
+	console.log('API Key: ' + apiKey);
+	console.log('Authorization URL: ' + authURL);
 	console.log('App server is running on http://localhost:' + port);
 });
+
+/*** FOR LOCAL SSL ONLY ***/
+/*
+https.createServer({
+  key: fs.readFileSync('ssl/key.pem'),
+  cert: fs.readFileSync('ssl/cert.pem')
+}, app).listen(port, function() {
+  //db.serialize(function() {
+    //db.run("CREATE TABLE token (username TEXT, token TEXT, refreshToken TEXT)");
+  //});
+	console.log('App server is running on https://localhost:' + port);
+});*/
