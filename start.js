51940bd03da1c06444efcc11055d039d3f34ba1a
ryan.veitch <ryan.r.veitch@gmail.com>
2016-12-09 23:13:30 -0600

    code formatting

--- start.js
+++ start.js
@@ -21,64 +21,68 @@ var authState = 'test';
 /******************************* EXPRESS ROUTES *******************************/
 
 // Default Endpoint
-app.get('/', function (req, res) {
-	var baseURL = req.protocol + '://' + req.headers.host;
-	var genericMessage = {
-		name: 'Destiny oAuth - Home',
-		url: baseURL,
-		endpoints: {
-			'/': baseURL + '/',
-			'/signin': baseURL + '/signin',
-			'/auth': baseURL + '/auth',
-		}
-	}
-	res.json(genericMessage);
+app.get('/', function(req, res) {
+  var baseURL = req.protocol + '://' + req.headers.host;
+  var genericMessage = {
+    name: 'Destiny oAuth - Home',
+    url: baseURL,
+    endpoints: {
+      '/': baseURL + '/',
+      '/signin': baseURL + '/signin',
+      '/auth': baseURL + '/auth',
+    }
+  }
+  res.json(genericMessage);
 });
 
 // Sign-In Endpoint (for Bungie Redirect)
-app.get('/signin', function (req, res) {
-	res.redirect(302, authURL + "?state="+ authState );
+app.get('/signin', function(req, res) {
+  res.redirect(302, authURL + "?state=" + authState);
 });
 
 // Auth Redirect Endpoint
-app.get('/auth', function (req, res) {
-	if (req.query.code) {
+app.get('/auth', function(req, res) {
+  if (req.query.code) {
 
-	  var responseCode = {
-	    code: req.query.code,
-	  }
+    var responseCode = {
+      code: req.query.code,
+    }
 
-	  request({
-	      url: 'https://www.bungie.net/Platform/App/GetAccessTokensFromCode/',
-	      method: 'POST',
-				headers: {
-					'Host': 'www.bungie.net',
-					'Origin': originHeader,
-			    'X-API-Key': apiKey,
-			  },
-				json: { code: req.query.code },
-	  }, function(error, response, body) {
-	      if (error) {
-					console.log('Error sending message: ', error);
-	      } else if (response.body.error) {
-					console.log('Error: ', response.body.error);
-	      } else {
-					console.log(response.body);
-					res.json(body);
-				}
-	  });
+    request({
+      url: 'https://www.bungie.net/Platform/App/GetAccessTokensFromCode/',
+      method: 'POST',
+      headers: {
+        'Host': 'www.bungie.net',
+        'Origin': originHeader,
+        'X-API-Key': apiKey,
+      },
+      json: {
+        code: req.query.code
+      },
+    }, function(error, response, body) {
+      if (error) {
+        console.log('Error sending message: ', error);
+      } else if (response.body.error) {
+        console.log('Error: ', response.body.error);
+      } else {
+        console.log(response.body);
+        res.json(body);
+      }
+    });
 
-	} else {
-		res.json({req.query});
-	}
+  } else {
+    res.json({
+      req.query
+    });
+  }
 });
 
 /******************************************************************************/
 
 // Server Listen
-app.listen(port, function () {
-	console.log('\nAPI Key: '.cyan + apiKey);
-	console.log('Authorization URL: '.cyan + authURL);
-	console.log('Origin Header: '.cyan + originHeader);
-	console.log('App server is running on ' + 'http://localhost:'.green + port.toString().green + '\n');
+app.listen(port, function() {
+  console.log('\nAPI Key: '.cyan + apiKey);
+  console.log('Authorization URL: '.cyan + authURL);
+  console.log('Origin Header: '.cyan + originHeader);
+  console.log('App server is running on ' + 'http://localhost:'.green + port.toString().green + '\n');
 });
