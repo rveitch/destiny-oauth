c960e8013b7d258223c022c4823caeaf9a3c0f8d
ryan.veitch <ryan.r.veitch@gmail.com>
2016-12-09 22:56:18 -0600

    v 0.1 update

--- README.md
+++ README.md
@@ -1,30 +1,23 @@
-destiny-oauth
+Destiny oAuth
+
+### FOR LOCAL SSL ONLY
+```javascript
+var fs = require('fs');
+var https = require('https'); // FOR LOCAL SSL ONLY
+
+https.createServer({
+  key: fs.readFileSync('ssl/key.pem'),
+  cert: fs.readFileSync('ssl/cert.pem')
+}, app).listen(port, function() {
+  //db.serialize(function() {
+    //db.run("CREATE TABLE token (username TEXT, token TEXT, refreshToken TEXT)");
+  //});
+	console.log('App server is running on https://localhost:' + port);
+});
+```
 
 #### Reference
+- https://www.bungie.net/Mobi/en/About/Article?id=45481
 - http://blog.mgechev.com/2014/02/19/create-https-tls-ssl-application-with-express-nodejs/
 - https://github.com/request/request/wiki/Using-request-with-https
 - https://github.com/request/request#tlsssl-protocol
-
-
-```json
-{
-  "Response": {
-    "accessToken": {
-      "value": "CHYS5gEAIL94FTchr7nxJtHk8+8cTHPtyz8Cdd5RJ32Kbe1N02AkwAAAAIODBq6vpcp0vpujly+VSdgIeeIP+2GP6/LM+CCi1CwvftFywOGBsD81DcRNFxc2fKJm/xP6KApKuROf6m5KLd3BkdTiHQaBDht8riaLH1RL2HklAO0MQ/k6tWqQKAhdMA6ce3q57XwAycdP7zxZR/pP+wmfGMn1gW5+86l+ctPmWJpcqhkuTOqFlft/KRnPB6g6UrXGi9c8zq3t5v4YD4cjbqfFDwZg+naM5DFdMElHIYwKgRtZOEhJ4dFieH3GaQ==",
-      "readyin": 0,
-      "expires": 3600
-    },
-    "refreshToken": {
-      "value": "CHYShgIAIPGdBMxfdnjLDGnjgXcC5w+8SkNHOwVxIWFWkYAgW8Zz4AAAAAfzPpziygQ/Had5eOVl37OPikZwgVG317QWFVezyVwu9wToVvmbnSTEQbbTfmUKtqCuXP3vOzTyuDtGuN2XGEwQLpyXUHYlPcK0O7ph69ZwDKzlhKI1j/RIs6JxF8ifAgKHe/dm/jrRjMiPJ0QB9PPIbXqIsMocfGI+55U6ZwytYbQxqLhTqMI47nQCbqhWD9DDk3kA/Hq6YFgWND4XbJTL31xWu8cMqi+2nPd3KgM5zF6RnwcfhDpbRTvFvp1rjICiSnAnVDR4uKDkr0jzvM0+0ZZsuOayus9CNeBED8uF",
-      "readyin": 1800,
-      "expires": 7776000
-    },
-    "scope": 225
-  },
-  "ErrorCode": 1,
-  "ThrottleSeconds": 0,
-  "ErrorStatus": "Success",
-  "Message": "Ok",
-  "MessageData": {}
-}
-```
--- start.js
+++ start.js
@@ -1,18 +1,13 @@
-#!/usr/bin/env node
 'use strict';
 
 var express = require('express');
 var favicon = require('serve-favicon');
 var request = require('request');
-var fs = require('fs');
 var sqlite3 = require('sqlite3').verbose();
 var colors = require('colors');
-//var https = require('https'); // FOR LOCAL SSL ONLY
-//var program = require('commander');
 
 var app = express();
 app.set('json spaces', 2); // Set Express to pretty print json
-//app.set('view engine', 'ejs'); // Set Express's view engine to EJS
 app.use(favicon(__dirname + '/public/favicon.ico'));
 var port = Number(process.env.PORT || 3000);
 
@@ -23,7 +18,7 @@ var originHeader = config.originHeader;
 var username, token, refreshToken;
 var authState = 'test';
 
-/******************** EXPRESS ROUTES ********************/
+/******************************* EXPRESS ROUTES *******************************/
 
 // Default Endpoint
 app.get('/', function (req, res) {
@@ -40,7 +35,7 @@ app.get('/', function (req, res) {
 	res.json(genericMessage);
 });
 
-// Sign-In Endpoint
+// Sign-In Endpoint (for Bungie Redirect)
 app.get('/signin', function (req, res) {
 	res.redirect(302, authURL + "?state="+ authState );
 });
@@ -48,43 +43,33 @@ app.get('/signin', function (req, res) {
 // Auth Redirect Endpoint
 app.get('/auth', function (req, res) {
 	if (req.query.code) {
-		console.log(req.query);
 
 	  var responseCode = {
 	    code: req.query.code,
 	  }
-		//var contentLength = responseCode;
-		console.log(responseCode);
 
-		//function sendMessage(recipientId, message) {
-		    request({
-		        url: 'https://www.bungie.net/Platform/App/GetAccessTokensFromCode/',
-		        method: 'POST',
-						headers: {
-							'Host': 'www.bungie.net',
-							//'Accept': 'application/json',
-							'Origin': originHeader,
-							//'User-Agent': 'app-platform',
-							//'Content-Length': contentLength.toString().length,
-					    'X-API-Key': apiKey,
-				      //'Content-Type': 'application/json; charset=UTF-8', // set by json parameter
-					  },
-						//body: JSON.stringify(post_data), //Set the body as a string
-						json: { code: req.query.code },
-		    }, function(error, response, body) {
-		        if (error) {
-		            console.log('Error sending message: ', error);
-		        } else if (response.body.error) {
-		            console.log('Error: ', response.body.error);
-		        } else {
-							console.log(response.body);
-							res.json(body);
-						}
-		    });
-		//};
-		//res.json(post_data);
+	  request({
+	      url: 'https://www.bungie.net/Platform/App/GetAccessTokensFromCode/',
+	      method: 'POST',
+				headers: {
+					'Host': 'www.bungie.net',
+					'Origin': originHeader,
+			    'X-API-Key': apiKey,
+			  },
+				json: { code: req.query.code },
+	  }, function(error, response, body) {
+	      if (error) {
+					console.log('Error sending message: ', error);
+	      } else if (response.body.error) {
+					console.log('Error: ', response.body.error);
+	      } else {
+					console.log(response.body);
+					res.json(body);
+				}
+	  });
+
 	} else {
-		res.json(req.query);
+		res.json({req.query});
 	}
 });
 
@@ -97,15 +82,3 @@ app.listen(port, function () {
 	console.log('Origin Header: '.cyan + originHeader);
 	console.log('App server is running on ' + 'http://localhost:'.green + port.toString().green + '\n');
 });
-
-/*** FOR LOCAL SSL ONLY ***/
-/*
-https.createServer({
-  key: fs.readFileSync('ssl/key.pem'),
-  cert: fs.readFileSync('ssl/cert.pem')
-}, app).listen(port, function() {
-  //db.serialize(function() {
-    //db.run("CREATE TABLE token (username TEXT, token TEXT, refreshToken TEXT)");
-  //});
-	console.log('App server is running on https://localhost:' + port);
-});*/
